1bcd3e897a1e68f5039b306798ffa3ae0663ab1c
zhangyueqingyun <zhangyueqingyun@foxmail.com>
2024-09-12 16:18:25 +0800

    feat(usercenter): add lombok dependency

--- src/main/java/priv/zhangyueqingyun/usercenter/orgnizationmember/OrgnizationMember.java
+++ src/main/java/priv/zhangyueqingyun/usercenter/orgnizationmember/OrgnizationMember.java
@@ -1,66 +1,29 @@
 package priv.zhangyueqingyun.usercenter.orgnizationmember;
 
 import priv.zhangyueqingyun.usercenter.user.User;
-
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
 @Entity 
 @Table(name = "user")
 public class OrgnizationMember extends User {
+  @Column(name = "orgnization_id")
   private String orgnizationId;
+  
+  @Column(name = "orgnization_name")
   private String orgnizationName;
+
+  @Column(name = "job_number")
   private String jobNumber;
+
+  @Column(name = "department_id")
   private String departmentId;
+
+  @Column(name = "department_name")
   private String departmentName;
-  private String role;
-  
-  
-  public String getOrgnizationId() {
-    return orgnizationId;
-  }
-
-  public void setOrgnizationId(String orgnizationId) {
-    this.orgnizationId = orgnizationId;
-  }
-
-  public String getOrgnizationName() {
-    return orgnizationName;
-  }
-
-  public void setOrgnizationName(String orgnizationName) {
-    this.orgnizationName = orgnizationName;
-  }
-
-  public String getJobNumber() {
-    return jobNumber;
-  }
-
-  public void setJobNumber(String jobNumber) {
-    this.jobNumber = jobNumber;
-  }
-
-  public String getDepartmentId() {
-    return departmentId;
-  }
-
-  public void setDepartmentId(String departmentId) {
-    this.departmentId = departmentId;
-  }
-
-  public String getDepartmentName() {
-    return departmentName;
-  }
-
-  public void setDepartmentName(String departmentName) {
-    this.departmentName = departmentName;
-  }
-
-  public String getRole() {
-    return role;
-  }
-
-  public void setRole(String role) {
-    this.role = role;
-  }
 }
--- src/main/java/priv/zhangyueqingyun/usercenter/systemuser/SystemUser.java
+++ src/main/java/priv/zhangyueqingyun/usercenter/systemuser/SystemUser.java
@@ -6,9 +6,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Date;
 
+@Getter
+@Setter
 @Entity 
 @Table(name = "user")
 public class SystemUser {
@@ -53,133 +57,4 @@ public class SystemUser {
   
   protected String device;
   protected String browser;
-
-  
-  public Integer getId() {
-    return id;
-  }
-
-  public void setId(Integer id) {
-    this.id = id;
-  }
-
-  public String getUsername() {
-    return username;
-  }
-
-  public void setUsername(String username) {
-    this.username = username;
-  }
-
-  public String getNickname() {
-    return nickname;
-  }
-
-  public void setNickname(String nickname) {
-    this.nickname = nickname;
-  }
-
-  public String getAvatar() {
-    return avatar;
-  }
-
-  public void setAvatar(String avatar) {
-    this.avatar = avatar;
-  }
-
-  public String getStatus() {
-    return status;
-  }
-
-  public void setStatus(String status) {
-    this.status = status;
-  }
-
-  public Date getCreateAt() {
-    return createAt;
-  }
-
-  public void setCreateAt(Date createAt) {
-    this.createAt = createAt;
-  }
-
-  public String getCreateBy() {
-    return createBy;
-  }
-
-  public void setCreateBy(String createBy) {
-    this.createBy = createBy;
-  }
-
-  public Date getUpdateAt() {
-    return updateAt;
-  }
-
-  public void setUpdateAt(Date updateAt) {
-    this.updateAt = updateAt;
-  }
-
-  public String getUpdateBy() {
-    return updateBy;
-  }
-
-  public void setUpdateBy(String updateBy) {
-    this.updateBy = updateBy;
-  }
-
-  public Integer getLoginCount() {
-    return this.loginCount;
-  }
-
-  public void setLoginCount(Integer loginCount) {
-    this.loginCount = loginCount;
-  }
-
-  public Date getLastLogin() {
-    return this.lastLogin;
-  }
-
-  public void setLastLogin(Date lastLogin) {
-    this.lastLogin = lastLogin;
-  }
-
-  public Boolean getSignedUp() {
-    return this.signedUp;
-  }
-
-  public void setSignedUp(Boolean signedUp) {
-    this.signedUp = signedUp;
-  }
-
-  public Boolean getBlocked() {
-    return blocked;
-  }
-
-  public void setBlocked(Boolean blocked) {
-    this.blocked = blocked;
-  }
-
-  public String getLastIp() {
-    return lastIp;
-  }
-
-  public void setLastIp(String lastIp) {
-    this.lastIp = lastIp;
-  }
-  
-  public String getDevice() {
-    return device;
-  }
-
-  public void setDevice(String device) {
-    this.device = device;
-  }
-
-  public String getBrowser() {
-    return browser;
-  }
-
-  public void setBrowser(String browser) {
-    this.browser = browser;
-  }
 }
